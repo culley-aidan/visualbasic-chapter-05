6eaf7fc6e224c8bebfd5a153bb940534cace4505
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-06 09:19:59 -0500

    added comments

--- London Tube Tickets/London Tube Tickets/Form1.vb
+++ London Tube Tickets/London Tube Tickets/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class frmLondonTubeTickets
+﻿' Program Name: London Tube tickets
+' Author:       Aidan J Culley
+' Date:         September 6th, 2019
+' Purpose:      This windows application computes the estimated cost of single fare tickets
+'               based on the number of tickets entered and what zone the ticket covers
 
+Public Class frmLondonTubeTickets
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+    End Sub
 End Class
