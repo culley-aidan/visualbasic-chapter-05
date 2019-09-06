a1ffa2b9f57bbb2e117d4b8457036c242b0033fc
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-06 08:11:56 -0500

    created grpBadges

--- Comic Convention/Comic Convention/Form1.Designer.vb
+++ Comic Convention/Comic Convention/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class frmComicConvention
         Me.lblHeading = New System.Windows.Forms.Label()
         Me.lblAttendees = New System.Windows.Forms.Label()
         Me.txtAttendees = New System.Windows.Forms.TextBox()
+        Me.grpBadges = New System.Windows.Forms.GroupBox()
         Me.SuspendLayout()
         '
         'lblHeading
@@ -43,7 +44,7 @@ Partial Class frmComicConvention
         Me.lblAttendees.AutoSize = True
         Me.lblAttendees.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblAttendees.ForeColor = System.Drawing.Color.Sienna
-        Me.lblAttendees.Location = New System.Drawing.Point(27, 74)
+        Me.lblAttendees.Location = New System.Drawing.Point(70, 74)
         Me.lblAttendees.Name = "lblAttendees"
         Me.lblAttendees.Size = New System.Drawing.Size(209, 25)
         Me.lblAttendees.TabIndex = 1
@@ -52,10 +53,22 @@ Partial Class frmComicConvention
         'txtAttendees
         '
         Me.txtAttendees.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtAttendees.Location = New System.Drawing.Point(242, 71)
+        Me.txtAttendees.Location = New System.Drawing.Point(285, 71)
         Me.txtAttendees.Name = "txtAttendees"
         Me.txtAttendees.Size = New System.Drawing.Size(58, 33)
         Me.txtAttendees.TabIndex = 2
+        '
+        'grpBadges
+        '
+        Me.grpBadges.BackColor = System.Drawing.Color.AliceBlue
+        Me.grpBadges.Font = New System.Drawing.Font("Goudy Old Style", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpBadges.ForeColor = System.Drawing.Color.Sienna
+        Me.grpBadges.Location = New System.Drawing.Point(56, 131)
+        Me.grpBadges.Name = "grpBadges"
+        Me.grpBadges.Size = New System.Drawing.Size(244, 106)
+        Me.grpBadges.TabIndex = 3
+        Me.grpBadges.TabStop = False
+        Me.grpBadges.Text = "Badge Type"
         '
         'frmComicConvention
         '
@@ -63,6 +76,7 @@ Partial Class frmComicConvention
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.grpBadges)
         Me.Controls.Add(Me.txtAttendees)
         Me.Controls.Add(Me.lblAttendees)
         Me.Controls.Add(Me.lblHeading)
@@ -76,4 +90,5 @@ Partial Class frmComicConvention
     Friend WithEvents lblHeading As Label
     Friend WithEvents lblAttendees As Label
     Friend WithEvents txtAttendees As TextBox
+    Friend WithEvents grpBadges As GroupBox
 End Class
