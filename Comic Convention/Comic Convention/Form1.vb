af0c5a05b11959fd76b555e2b70b838df0044219
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-06 08:34:53 -0500

    turned on strict type checking

--- Comic Convention/Comic Convention/Form1.vb
+++ Comic Convention/Comic Convention/Form1.vb
@@ -4,5 +4,6 @@
 ' Purpose:      This windows application computes the estimated cost of group convention
 '               tickets ased on what badge the group is using
 
+Option Strict On
 Public Class frmComicConvention
 End Class
