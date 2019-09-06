986e3c5258e187a724e52d64e550bcb699126ab9
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-06 08:34:39 -0500

    made comments

--- Comic Convention/Comic Convention/Form1.vb
+++ Comic Convention/Comic Convention/Form1.vb
@@ -1,3 +1,8 @@
-﻿Public Class frmComicConvention
+﻿' Program Name: Comic Convention
+' Author:       Aidan J Culley
+' Date:         September 6th, 2019
+' Purpose:      This windows application computes the estimated cost of group convention
+'               tickets ased on what badge the group is using
 
+Public Class frmComicConvention
 End Class
