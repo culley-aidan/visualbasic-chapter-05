dc2438f259316a28e4a467cab3c852c347048893
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-06 09:20:21 -0500

    turned on strict type checking

--- London Tube Tickets/London Tube Tickets/Form1.vb
+++ London Tube Tickets/London Tube Tickets/Form1.vb
@@ -4,6 +4,7 @@
 ' Purpose:      This windows application computes the estimated cost of single fare tickets
 '               based on the number of tickets entered and what zone the ticket covers
 
+Option Strict On
 Public Class frmLondonTubeTickets
     Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
 
