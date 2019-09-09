21e5daf1d224391b8f6131357c38747799620331
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-09 08:58:27 -0500

    created grpDiscounts

--- Broadway Ticket Group Discount/Broadway Ticket Group Discount/Form1.Designer.vb
+++ Broadway Ticket Group Discount/Broadway Ticket Group Discount/Form1.Designer.vb
@@ -29,6 +29,7 @@ Partial Class frmBroadwayDiscount
         Me.lblTotalCost = New System.Windows.Forms.Label()
         Me.btnCalculate = New System.Windows.Forms.Button()
         Me.btnClear = New System.Windows.Forms.Button()
+        Me.grpDiscounts = New System.Windows.Forms.GroupBox()
         Me.SuspendLayout()
         '
         'lblHeading
@@ -66,7 +67,7 @@ Partial Class frmBroadwayDiscount
         Me.lblCost.AutoSize = True
         Me.lblCost.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblCost.ForeColor = System.Drawing.Color.Sienna
-        Me.lblCost.Location = New System.Drawing.Point(14, 130)
+        Me.lblCost.Location = New System.Drawing.Point(69, 290)
         Me.lblCost.Name = "lblCost"
         Me.lblCost.Size = New System.Drawing.Size(110, 25)
         Me.lblCost.TabIndex = 3
@@ -77,7 +78,7 @@ Partial Class frmBroadwayDiscount
         Me.lblTotalCost.AutoSize = True
         Me.lblTotalCost.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lblTotalCost.ForeColor = System.Drawing.Color.Sienna
-        Me.lblTotalCost.Location = New System.Drawing.Point(130, 130)
+        Me.lblTotalCost.Location = New System.Drawing.Point(275, 290)
         Me.lblTotalCost.Name = "lblTotalCost"
         Me.lblTotalCost.Size = New System.Drawing.Size(93, 25)
         Me.lblTotalCost.TabIndex = 4
@@ -87,7 +88,7 @@ Partial Class frmBroadwayDiscount
         '
         Me.btnCalculate.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnCalculate.ForeColor = System.Drawing.Color.Sienna
-        Me.btnCalculate.Location = New System.Drawing.Point(19, 181)
+        Me.btnCalculate.Location = New System.Drawing.Point(74, 339)
         Me.btnCalculate.Name = "btnCalculate"
         Me.btnCalculate.Size = New System.Drawing.Size(118, 40)
         Me.btnCalculate.TabIndex = 5
@@ -98,19 +99,32 @@ Partial Class frmBroadwayDiscount
         '
         Me.btnClear.Font = New System.Drawing.Font("Goudy Old Style", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnClear.ForeColor = System.Drawing.Color.Sienna
-        Me.btnClear.Location = New System.Drawing.Point(175, 181)
+        Me.btnClear.Location = New System.Drawing.Point(250, 339)
         Me.btnClear.Name = "btnClear"
         Me.btnClear.Size = New System.Drawing.Size(118, 40)
         Me.btnClear.TabIndex = 6
         Me.btnClear.Text = "Clear"
         Me.btnClear.UseVisualStyleBackColor = True
         '
+        'grpDiscounts
+        '
+        Me.grpDiscounts.BackColor = System.Drawing.Color.AliceBlue
+        Me.grpDiscounts.Font = New System.Drawing.Font("Goudy Old Style", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpDiscounts.ForeColor = System.Drawing.Color.Sienna
+        Me.grpDiscounts.Location = New System.Drawing.Point(74, 121)
+        Me.grpDiscounts.Name = "grpDiscounts"
+        Me.grpDiscounts.Size = New System.Drawing.Size(294, 146)
+        Me.grpDiscounts.TabIndex = 7
+        Me.grpDiscounts.TabStop = False
+        Me.grpDiscounts.Text = "Discounts"
+        '
         'frmBroadwayDiscount
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(458, 450)
+        Me.Controls.Add(Me.grpDiscounts)
         Me.Controls.Add(Me.btnClear)
         Me.Controls.Add(Me.btnCalculate)
         Me.Controls.Add(Me.lblTotalCost)
@@ -132,4 +146,5 @@ Partial Class frmBroadwayDiscount
     Friend WithEvents lblTotalCost As Label
     Friend WithEvents btnCalculate As Button
     Friend WithEvents btnClear As Button
+    Friend WithEvents grpDiscounts As GroupBox
 End Class
