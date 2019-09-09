e18ecbd66cecfceb38c70dcff55c6819bf1cc97c
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-09 09:23:09 -0500

    added btnCalculate logic

--- Broadway Ticket Group Discount/Broadway Ticket Group Discount/Form1.vb
+++ Broadway Ticket Group Discount/Broadway Ticket Group Discount/Form1.vb
@@ -13,6 +13,33 @@ Public Class frmBroadwayDiscount
     Const intGroup2599 As Integer = 169
 
     Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim totalTickets As Integer
+        Dim discount As Integer
+        Dim totalCost As Integer
 
+        If IsNumeric(txtGroupCount.Text) Then
+            totalTickets = Convert.ToInt32(txtGroupCount.Text)
+            If totalTickets >= 1 And totalTickets < 100 Then
+                Select Case totalTickets
+                    Case 1 To 8
+                        discount = intGroup18
+                    Case 9 To 12
+                        discount = intGroup912
+                    Case 13 To 24
+                        discount = intGroup1324
+                    Case 25 To 99
+                        discount = intGroup2599
+                End Select
+                totalCost = totalTickets * discount
+                lblTotalCost.Text = totalCost.ToString("C")
+            Else
+                MsgBox("You entered " & totalTickets.ToString() & ", Enter a positive number between 1-100", , "Input Error")
+            End If
+        Else
+            ' Display error message if user entered a nonnumeric value
+            MsgBox("Enter the total amount of tickets", , "Input Error")
+            txtGroupCount.Text = ""
+            txtGroupCount.Focus()
+        End If
     End Sub
 End Class
