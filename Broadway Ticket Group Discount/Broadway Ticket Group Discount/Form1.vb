8abd61deaaed063bb35cfacc83ec44e9b5ae83ac
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-09 09:05:20 -0500

    added strict type checking

--- Broadway Ticket Group Discount/Broadway Ticket Group Discount/Form1.vb
+++ Broadway Ticket Group Discount/Broadway Ticket Group Discount/Form1.vb
@@ -4,6 +4,7 @@
 ' Purpose:      This windows application computes the estimated cost of tickets based on the 
 '               number  of tickets purchased and the bulk discounts of tickets
 
+Option Strict On
 Public Class frmBroadwayDiscount
     Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
 
