e63f1905a5313a6ffa6fdb573420e158292aa44d
culley-aidan <culley-aidan@users.noreply.github.com>
2019-09-09 09:05:06 -0500

    added introductory line

--- Broadway Ticket Group Discount/Broadway Ticket Group Discount/Form1.vb
+++ Broadway Ticket Group Discount/Broadway Ticket Group Discount/Form1.vb
@@ -1,3 +1,11 @@
-﻿Public Class frmBroadwayDiscount
+﻿' Program Name: Broadway Ticket Group Discount
+' Author:       Aidan J, Culley
+' Date:         September 9th, 2019
+' Purpose:      This windows application computes the estimated cost of tickets based on the 
+'               number  of tickets purchased and the bulk discounts of tickets
 
+Public Class frmBroadwayDiscount
+    Private Sub BtnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+    End Sub
 End Class
